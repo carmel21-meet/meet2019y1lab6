ca3b91aade442c647d056e18b6166b07c2537ea8
carmel21-meet <carmel21@meet.mit.edu>
2019-07-28 09:29:35 -0400

    Stopping here

--- lab_6.py
+++ lab_6.py
@@ -1,9 +1,8 @@
-result = []
-n = 0
-for count in range(1, n):
-    if count % 3 == 0:
-        result.appent("Fizz")
-    else:
-        result.append(count)
-    
-            
+def add_numbers():
+    total = 0
+    for number in range(1,10 + 1):
+        print(number)
+        total = total + number
+    print(total)
+answer = add_numbers()
+print(answer)
